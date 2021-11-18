dd2a13effbed51975749cc4f2527eb8c10ff1129
Odd Andreas Sørsæther <odd.andreas.sorsether@nav.no>
2021-11-18 12:07:01 +0100

    Dag 1

---
+++ src/advent_of_code_clj/dag1.clj
@@ -0,0 +1,39 @@
+(ns advent-of-code-clj.dag1
+  (:require [clojure.string :as str]))
+
+(comment
+  (def liste-av-tall (map #(Integer/parseInt %)
+                          (str/split-lines (slurp "input/day1-input.txt"))))
+
+  ; Multipliser to tall som i sum er lik 2020
+  (loop [[x & resten :as listen] liste-av-tall]
+    (if (nil? listen)
+      (throw (ex-info "Gikk tom for tall" {}))
+      (if-let [y (->> resten
+                      (filter #(= 2020 (+ x %)))
+                      first)]
+        (* x y)
+        (recur resten))))
+
+  ; Multipliser tre tall som i sum er lik 2020
+  (loop [[x & rest-1 :as liste] liste-av-tall]
+    (if (nil? liste)
+      (throw (ex-info "Gikk tom for tall" {}))
+      (if-let [multiplert (loop [[y & rest-2 :as liste] rest-1]
+                            (if (nil? liste)
+                              nil
+                              (if-let [z (->> rest-2
+                                              (filter #(= 2020 (+ x y %)))
+                                              first)]
+                                (* x y z)
+                                (recur rest-2))))]
+        multiplert
+        (recur rest-1))))
+  
+  ; Multipliser to tall som i sum er lik 2020
+  ; NB: Fungerer ikke; x og y kan være samme nummer
+  (first (for [x liste-av-tall
+               y (drop 1 liste-av-tall)
+               :when (= 2020 (+ x y))]
+           (* x y)))
+  )
