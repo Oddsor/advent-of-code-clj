934f202bed562e97cc7eac0a138d5ea9af8b149d
Odd Andreas Sørsæther <odd.andreas.sorsether@nav.no>
2021-11-18 15:03:19 +0100

    Dag 6

---
+++ src/advent_of_code_clj/dag6.clj
@@ -0,0 +1,67 @@
+(ns advent-of-code-clj.dag6
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def test-data-del-1 "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+(def test-data-del-2 "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+(defn antall-besvarte-spørsmål-i-gruppe [gruppe]
+  (count (set (mapcat identity gruppe))))
+
+(defn antall-spørsmål-besvart-av-hele-gruppe [gruppe]
+  (count (apply set/union (map set gruppe))))
+
+(defn del-1 [data]
+  (let [linjer (str/split-lines data)
+        grupperte-svar (->> linjer
+                            (partition-by #{""})
+                            (remove #{'("")}))]
+    (reduce + (map antall-besvarte-spørsmål-i-gruppe grupperte-svar))))
+
+(defn del-2 [data]
+  (let [linjer (str/split-lines data)
+        grupperte-svar (->> linjer
+                            (partition-by #{""})
+                            (remove #{'("")}))]
+    (reduce + (map antall-besvarte-spørsmål-i-gruppe grupperte-svar))))
+
+(= 11 (del-1 test-data-del-1))
+
+(= 11 (del-2 test-data-del-2))
+
+(comment
+  ; Del 1
+  (process (slurp "input/day6-input.txt"))
+
+  ; Del 2
+
+  )
