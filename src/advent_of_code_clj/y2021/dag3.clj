bdabbf744fc8d60f0033dc20de7efd0e4749c622
Odd Andreas Sørsæther <odd.andreas.sorsether@nav.no>
2021-12-03 10:38:51 +0100

    Forbedret lesbarhet

--- src/advent_of_code_clj/y2021/dag3.clj
+++ src/advent_of_code_clj/y2021/dag3.clj
@@ -18,26 +18,26 @@
   (str/split-lines text))
 
 (defn bit-str-to-dec [bit-str]
-  (read-string (str "2r" bit-str)))
+  (when (re-matches #"^[10]*$" bit-str)
+    (read-string (str "2r" bit-str))))
+
+(defn transpose
+  "(transpose [[1 2] [3 4]])
+   => '((1 3) (2 4]))"
+  [seq-of-seqs]
+  (apply map list seq-of-seqs))
 
 (defn calculate-power-consumption [data]
   (->> data
-       (apply map list)
-       (map frequencies)
-       (map (partial sort-by val))
-       (map (juxt (comp first second)
-                  ffirst))
-       ((juxt (comp bit-str-to-dec
-                    (partial apply str)
-                    (partial map first))
-              (comp bit-str-to-dec
-                    (partial apply str)
-                    (partial map second))))
+       transpose
+       (map #(->> % frequencies (sort-by val) (map first)))
+       transpose
+       (map #(->> % (apply str) bit-str-to-dec))
        (apply *)))
 
 (assert (= 198 (calculate-power-consumption (parse test-data))))
 (comment
-  (calculate-power-consumption (parse (slurp "input/y2021/day3-input.txt"))))
+  (= 3901196 (calculate-power-consumption (parse (slurp "input/y2021/day3-input.txt")))))
 
 (defn narrow-bits-by-fn [fun data]
   (reduce (fn [data idx]
@@ -50,11 +50,12 @@
                                 frequencies
                                 (sort (fn [a b]
                                         (fun (val a) (val b)))))
-                    to-take (if (every? #{(-> sorted first second)} (map second sorted))
+                    to-take (if (apply = (map second sorted))
                               preferred-bit
                               (ffirst sorted))]
-                (filter (comp #{to-take} #(nth % idx)) data)))) 
-          data (range (count (first data)))))
+                (filter #(= to-take (nth % idx)) data))))
+          data 
+          (range (count (first data)))))
 
 (assert (= 230 (let [data (parse test-data)]
                  (* (bit-str-to-dec (first (narrow-bits-by-fn > data)))
@@ -62,5 +63,5 @@
 
 (comment
   (let [data (parse (slurp "input/y2021/day3-input.txt"))]
-    (* (bit-str-to-dec (first (narrow-bits-by-fn > data)))
-       (bit-str-to-dec (first (narrow-bits-by-fn < data))))))
+    (= 4412188 (* (bit-str-to-dec (first (narrow-bits-by-fn > data)))
+                  (bit-str-to-dec (first (narrow-bits-by-fn < data)))))))
