9c07f5e4a3991f9e49db53aafd32eb16ac7cf1d3
Odd Andreas Sørsæther <odd.andreas.sorsether@nav.no>
2021-12-09 11:50:51 +0100

    expand-outward

--- src/advent_of_code_clj/y2021/dag9.clj
+++ src/advent_of_code_clj/y2021/dag9.clj
@@ -20,8 +20,9 @@
   (assert (vector-matrix? data)
           "Må være vektormatrise for at get-in skal fungere!")
   (->> [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]]
-       (map (fn [xs]
-              [xs (get-in data (reverse xs) nil)]))
+       (keep (fn [xs]
+               (when-let [v (get-in data (reverse xs) nil)]
+                 [xs v])))
        (into {})))
 
 (defn find-minima [data]
@@ -29,31 +30,38 @@
   (for [y (range (count data))
         x (range (count (first data)))
         :let [height (get-in data [y x])
-              adjacents (->> (adjacent x y data)
-                             vals
-                             (remove nil?))]
+              adjacents (vals (adjacent x y data))]
         :when (and (= height (apply min height adjacents))
                    (not-any? #{height} adjacents))]
     [x y (inc height)]))
 
-(def valid-basin-height? (complement (some-fn #{9} nil?)))
+(def peak? #{9})
 
-(defn find-adjacent-higher [x y data]
-  (let [height (get-in data [y x])
-        higher-adjacents (->> (adjacent x y data)
-                              (filter (comp valid-basin-height? val))
-                              (filter #(> (val %) height)))]
-    (cons [x y height]
-          (mapcat (fn [[x y]]
-                    (find-adjacent-higher x y data))
-                  (keys higher-adjacents)))))
+(defn higher-adjacents [x y data]
+  (let [height (get-in data [y x])]
+    (->> (adjacent x y data)
+         (remove (comp peak? val))
+         (filter #(> (val %) height))
+         keys)))
+
+(defn expand-outward [plots data]
+  (lazy-seq
+   (if-let [xs (seq (mapcat (fn [[x y]]
+                              (higher-adjacents x y data)) plots))]
+     (cons plots (expand-outward xs data))
+     [plots])))
+
+(defn collect-plots [plot-groups]
+  (set (mapcat identity plot-groups)))
 
 (assert (= 15 (apply + (map last (find-minima test-data)))))
 
 (assert (= 1134 (->> test-data
                      find-minima
-                     (map (fn [[x y]]
-                            (count (set (find-adjacent-higher x y test-data)))))
+                     (map (fn [xs]
+                            (-> (expand-outward [xs] test-data)
+                                collect-plots
+                                count)))
                      sort
                      (take-last 3)
                      (apply *))))
@@ -63,13 +71,17 @@
   (->> (parse-lines (slurp "input/y2021/day9-input.txt"))
        find-minima
        (map last)
-       (apply +))
+       (apply +)
+       (= 444))
   ;; Part 2
   (let [data (parse-lines (slurp "input/y2021/day9-input.txt"))]
     (->> data
          find-minima
-         (map (fn [[x y]]
-                (count (set (find-adjacent-higher x y data)))))
+         (map (fn [plot]
+                (-> (expand-outward [plot] data)
+                    collect-plots
+                    count)))
          sort
          (take-last 3)
-         (apply *))))
+         (apply *)
+         (= 1168440))))
