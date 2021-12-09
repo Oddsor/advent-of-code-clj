31f6539ac8a5e4460f0b834d4163b68b3a860bf1
Odd Andreas Sørsæther <odd.andreas.sorsether@nav.no>
2021-12-09 15:07:22 +0100

    Y2021 Dag 9 med coord-map

---
+++ src/advent_of_code_clj/y2021/dag9-map.clj
@@ -0,0 +1,82 @@
+(ns advent-of-code-clj.y2021.dag9-map
+  (:require [clojure.string :as str]))
+
+(defn parse-nums [idy text]
+  (apply merge
+         (map-indexed (fn [idx t]
+                        {[idx idy] (read-string t)}) (re-seq #"\d" text))))
+(defn parse-lines [text]
+  (apply merge
+         (map-indexed (partial parse-nums) (str/split-lines text))))
+
+(def test-data (parse-lines "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"))
+
+(defn adjacent [x y]
+  [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]])
+
+(defn find-minima [data]
+  (filter (fn [[point value]]
+            (< value
+               (apply min (keep data (apply adjacent point)))))
+          data))
+
+(defn higher-adjacent-points [data point]
+  (->> (apply adjacent point)
+       (select-keys data)
+       (filter #(> 9 (val %) (data point)))
+       keys))
+
+(defn climb [data points]
+  (lazy-seq
+   (if-let [xs (seq (mapcat (partial higher-adjacent-points data) points))]
+     (cons points (climb data xs))
+     [points])))
+
+(comment (climb test-data [[9 0]]))
+;; ([[9 0]] 
+;;  ([8 0] [9 1])
+;;  ([7 0] [8 1] [8 1] [9 2])
+;;  ([6 0])
+;;  ([5 0] [6 1]))
+;; 2199943210      0000054321 
+;; 3987894921      0000005032
+;; 9856789892  ->  0000000003
+;; 8767896789      0000000000
+;; 9899965678      0000000000
+
+(defn area [plot-groups]
+  (count (set (mapcat identity plot-groups))))
+
+(assert (= 15 (apply + (map (comp inc val) (find-minima test-data)))))
+
+(assert (= 1134 (->> test-data
+                     find-minima
+                     keys
+                     (map (fn [xs]
+                            (->> xs
+                                 vector
+                                 (climb test-data)
+                                 area)))
+                     sort
+                     (take-last 3)
+                     (apply *))))
+
+(comment
+  (time (let [data (parse-lines (slurp "input/y2021/day9-input.txt"))
+              minima (find-minima data)]
+          [(->> minima
+                (map (comp inc val))
+                (apply +)
+                (= 444))
+           (->> minima
+                keys
+                (map (fn [plot]
+                       (area (climb data [plot]))))
+                (sort >)
+                (take 3)
+                (apply *)
+                (= 1168440))])))
