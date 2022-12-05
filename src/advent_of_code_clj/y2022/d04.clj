c166817b83d9154946800de19e994007d7f51a2c
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-05 10:44:26 +0100

    Y2022D04 tests

--- src/advent_of_code_clj/y2022/d04.clj
+++ src/advent_of_code_clj/y2022/d04.clj
@@ -1,13 +1,6 @@
 (ns advent-of-code-clj.y2022.d04 
   (:require [clojure.set :as set]))
 
-(def test-data "2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8")
-
 (defn overlaps [[a b c d]]
   (or (<= a c d b)
       (<= c a b d)))
@@ -33,8 +26,6 @@
        (filter overlaps-at-all)
        count))
 
-(assert (= 2 (part-1 test-data)))
-(assert (= 4 (part-2 test-data)))
 (comment 
   (part-1 (slurp "input/2022/04.txt"))
   (part-2 (slurp "input/2022/04.txt")))
---
+++ test/advent_of_code_clj/y2022/d04_test.clj
@@ -0,0 +1,15 @@
+(ns advent-of-code-clj.y2022.d04-test 
+  (:require [advent-of-code-clj.y2022.d04 :refer [part-1 part-2]]
+            [clojure.test :refer [deftest is]]))
+
+(def test-data "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8")
+
+(deftest part-1-test
+  (is (= 2 (part-1 test-data))))
+(deftest part-2-test
+  (is (= 4 (part-2 test-data))))
