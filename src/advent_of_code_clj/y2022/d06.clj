ab00ab71f922e9d40209708b2ddbbbdca1aecd26
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-06 10:21:23 +0100

    Y2022D06 - faster and more readable
    Was reminded that the 'step'-argument exists for `partition`.
    Also `distinct?` apparently exists and is pretty fast

--- src/advent_of_code_clj/y2022/d06.clj
+++ src/advent_of_code_clj/y2022/d06.clj
@@ -1,12 +1,10 @@
 (ns advent-of-code-clj.y2022.d06)
 
 (defn unique-sequence-end-index [length data]
-  (let [data-vec (vec data)]
-    (reduce (fn [acc n]
-              (if (= length (count (set (subvec data-vec (- n length) n))))
-                (reduced n)
-                acc))
-            nil (range length (count data-vec)))))
+  (->> data
+       (partition length 1)
+       (take-while (partial apply (complement distinct?)))
+       count (+ length)))
 
 (defn part-1 [data]
   (unique-sequence-end-index 4 data))
