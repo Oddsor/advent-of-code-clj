9b0acc5731c8f09f203bfd356bbba3b3c5dd732c
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-06 10:52:02 +0100

    Y2022D06 - testing dtype-next library
    Using the fixed-rolling-window function to generate partitions and
    applying a function to the sub-sequences.
    
    Some quick benchmarking indicates a decent performance
    boost without optimizations

--- src/advent_of_code_clj/y2022/d06.clj
+++ src/advent_of_code_clj/y2022/d06.clj
@@ -1,4 +1,7 @@
-(ns advent-of-code-clj.y2022.d06)
+(ns advent-of-code-clj.y2022.d06
+  (:require [tech.v3.datatype.argops :as da] 
+            [tech.v3.datatype.rolling :as dr]
+            [criterium.core :as crit]))
 
 (defn unique-sequence-end-index [length data]
   (->> data
@@ -13,4 +16,29 @@
   (unique-sequence-end-index 14 data))
 
 (comment (part-1 (slurp "input/2022/06.txt"))
-         (part-2 (slurp "input/2022/06.txt")))
+         (crit/quick-bench
+          (part-2 (slurp "input/2022/06.txt")))
+         ;; Execution time mean : 10,199115 ms
+         )
+
+
+(defn unique-sequence-end-index-2 [length data]
+  (-> (seq data)
+      (dr/fixed-rolling-window length 
+                               (partial apply distinct?) 
+                               {:relative-window-position :right})
+      (da/index-of true)
+      (+ length)))
+
+(defn part-1-dt [data]
+  (unique-sequence-end-index-2 4 data))
+
+(defn part-2-dt [data]
+  (unique-sequence-end-index-2 14 data))
+
+(comment 
+  (part-1-dt (slurp "input/2022/06.txt"))
+  (crit/quick-bench 
+   (part-2-dt (slurp "input/2022/06.txt")))
+  ;; Execution time mean : 5,866074 ms
+  )
--- test/advent_of_code_clj/y2022/d06_test.clj
+++ test/advent_of_code_clj/y2022/d06_test.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-clj.y2022.d06-test 
-  (:require [advent-of-code-clj.y2022.d06 :refer [part-1 part-2]]
+  (:require [advent-of-code-clj.y2022.d06 :refer [part-1 part-1-dt part-2
+                                                  part-2-dt]]
             [clojure.test :refer [deftest is]]))
 
 (def test-1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
@@ -21,3 +22,17 @@
   (is (= 23 (part-2 test-3)))
   (is (= 29 (part-2 test-4)))
   (is (= 26 (part-2 test-5))))
+
+(deftest part-1-rolling-test
+  (is (= 7 (part-1-dt test-1)))
+  (is (= 5 (part-1-dt test-2)))
+  (is (= 6 (part-1-dt test-3)))
+  (is (= 10 (part-1-dt test-4)))
+  (is (= 11 (part-1-dt test-5))))
+
+(deftest part-2-rolling-test
+  (is (= 19 (part-2-dt test-1)))
+  (is (= 23 (part-2-dt test-2)))
+  (is (= 23 (part-2-dt test-3)))
+  (is (= 29 (part-2-dt test-4)))
+  (is (= 26 (part-2-dt test-5))))
