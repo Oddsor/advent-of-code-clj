24ce994125d0373525c16da663534e1cc7868a4e
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-08 09:16:54 +0100

    Y2022D08 refactoring, less bad

--- src/advent_of_code_clj/y2022/d08.clj
+++ src/advent_of_code_clj/y2022/d08.clj
@@ -2,33 +2,25 @@
   (:require [clojure.string :as str]))
 
 (defn- build-matrix [data]
-  (->> data str/split-lines vec (mapv #(vec (map parse-long (re-seq #"\d" %))))))
-(defn- visible-trees-along-edges [m]
-  (+ (* (count m) 2)
-     (* (- (count (first m)) 2) 2)))
+  (->> data str/split-lines (mapv #(mapv parse-long (re-seq #"\d" %)))))
+
+(defn coordinates-from-point [x y width height]
+  (vector (for [xn (range (dec x) -1 -1)] [y xn])
+          (for [yn (range (dec y) -1 -1)] [yn x])
+          (for [xp (range (inc x) width)] [y xp])
+          (for [yp (range (inc y) height)] [yp x])))
 
 (defn part-1 [data]
   (let [m (build-matrix data)
         height (count m)
         width (count (first m))]
-    (->> (for [x (range 1 (dec height))
-               y (range 1 (dec width))
-               :let [tree (-> m (nth y) (nth x))]]
-           (or (every? #(> tree (get-in m %))
-                       (for [xn (range (dec x) -1 -1)]
-                         [y xn]))
-               (every? #(> tree (get-in m %))
-                       (for [yn (range (dec y) -1 -1)]
-                         [yn x]))
-               (every? #(> tree (get-in m %))
-                       (for [xp (range (inc x) width)]
-                         [y xp]))
-               (every? #(> tree (get-in m %))
-                       (for [yp (range (inc y) height)]
-                         [yp x]))))
-         (filter true?)
-         count
-         (+ (visible-trees-along-edges m)))))
+    (->> (for [x (range height)
+               y (range width)
+               :let [tree (get-in m [y x])
+                     lower-tree? #(> tree (get-in m %))]]
+           (some #(every? lower-tree? %) (coordinates-from-point x y width height)))
+         (remove nil?)
+         count)))
 
 (defn part-2 [data]
   (let [m (build-matrix data)
@@ -36,23 +28,13 @@
         width (count (first m))]
     (->> (for [x (range height)
                y (range width)
-               :let [tree (-> m (nth y) (nth x))
+               :let [tree (get-in m [y x])
                      add-if-visible (fn [acc coords]
                                       (if (> tree (get-in m coords))
                                         (inc acc)
                                         (reduced (inc acc))))]]
-           (* (reduce add-if-visible 0
-                      (for [xn (range (dec x) -1 -1)]
-                        [y xn]))
-              (reduce add-if-visible 0
-                      (for [yn (range (dec y) -1 -1)]
-                        [yn x]))
-              (reduce add-if-visible 0
-                      (for [xp (range (inc x) width)]
-                        [y xp]))
-              (reduce add-if-visible 0
-                      (for [yp (range (inc y) height)]
-                        [yp x]))))
+           (apply * (map #(reduce add-if-visible 0 %)
+                         (coordinates-from-point x y width height))))
          (apply max))))
 
 (comment
