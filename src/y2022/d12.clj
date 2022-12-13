cff85bd101c34d1dbe7b4abd22bfe3c6d01a80b6
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-13 19:54:53 +0100

    Y2022D12 refactor

--- src/y2022/d12.clj
+++ src/y2022/d12.clj
@@ -1,32 +1,26 @@
-(ns y2022.d12 
-  (:require [clojure.string :as str] 
+(ns y2022.d12
+  (:require [clojure.string :as str]
             [ubergraph.alg :as ua]))
 
 (defn parse [data]
-  (let [m (->> data str/split-lines (mapv (comp vec seq)))
-        st (into {} (for [y (range (count m))
-                          x (range (count (first m)))
-                          :let [position (get-in m [y x])]
-                          :when (#{\S \E} position)]
-                      [position [y x]]))]
-    {:start (st \S)
-     :end (st \E)
+  (let [m (->> data str/split-lines (mapv vec))
+        s&e (into {} (for [y (range (count m))
+                           x (range (count (first m)))
+                           :let [position (get-in m [y x])]
+                           :when (#{\S \E} position)]
+                       [position [y x]]))]
+    {:start (s&e \S)
+     :end (s&e \E)
      :map (mapv #(mapv int %) (-> m
-                                  (assoc-in (st \S) \a)
-                                  (assoc-in (st \E) \z)))}))
-
-(defn adjacent [x y data] 
-  (->> [[y (dec x)] [y (inc x)] [(dec y) x] [(inc y) x]]
-       (keep (fn [xs]
-               (when-let [v (get-in data xs nil)]
-                 [xs v])))
-       (into {})))
+                                  (assoc-in (s&e \S) \a)
+                                  (assoc-in (s&e \E) \z)))}))
 
 (defn valid-adjacents [[y x] data]
   (let [height (get-in data [y x])]
-    (->> (adjacent x y data)
-         (remove #(> (val %) (inc height))) 
-         keys)))
+    (->> [[y (dec x)] [y (inc x)] [(dec y) x] [(inc y) x]]
+         (filter (fn [coord]
+                   (when-let [nearby-height (get-in data coord nil)]
+                     (>= (inc height) nearby-height)))))))
 
 (defn shortest-path [from to m]
   (:cost (ua/shortest-path (fn [node]
@@ -36,20 +30,18 @@
                             :end-node to})))
 
 (defn part-1 [data]
-  (let [state (parse data)
-        m (:map state)
-        s (:start state)
-        e (:end state)]
-    (shortest-path s e m)))
+  (let [state (parse data)]
+    (shortest-path (:start state) (:end state) (:map state))))
 
 (defn part-2 [data]
   (let [state (parse data)
-        m (:map state)
-        e (:end state)]
-    (apply min (keep #(shortest-path % e m) (for [y (range (count m))
-                                                  x (range (count (first m)))
-                                                  :when (= (int \a) (get-in m [y x]))]
-                                              [y x])))))
+        m (:map state)]
+    (->> (for [y (range (count m))
+               x (range (count (first m)))
+               :when (= (int \a) (get-in m [y x]))]
+           [y x])
+         (keep #(shortest-path % (:end state) m))
+         (apply min))))
 
 (comment (part-1 (slurp "input/2022/12.txt"))
          (part-2 (slurp "input/2022/12.txt")))
