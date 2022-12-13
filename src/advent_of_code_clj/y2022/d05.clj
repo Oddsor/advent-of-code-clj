f862389b42db9cb09e945362f7172c3d217056b7
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-13 19:59:07 +0100

    Y2022D05 simplify stack-building

--- src/advent_of_code_clj/y2022/d05.clj
+++ src/advent_of_code_clj/y2022/d05.clj
@@ -5,38 +5,33 @@
   (->> stack-str
        str/split-lines
        (drop-last 1)
-       (map (fn [line]
-              (partition-all 4 line)))
-       (map (partial map (fn [group]
-                           (remove #{\space \[ \]} group))))
-       (map (partial map first))
-       reverse
-       (apply mapv vector)
-       (mapv (partial filterv some?))))
+       (map #(take-nth 4 (rest %)))
+       (apply mapv list)
+       (mapv #(drop-while #{\space} %))))
 
 (defn operate-on-one [stacks [move-amount from to]]
   (loop [stacks stacks
          n move-amount]
     (if (zero? n)
       stacks
-      (let [popped (last (stacks (dec from)))]
+      (let [popped (first (stacks (dec from)))]
         (recur (-> stacks
-                   (update (dec from) #(vec (drop-last 1 %)))
+                   (update (dec from) rest)
                    (update (dec to) conj popped))
                (dec n))))))
 
 (defn operate-on-many [stacks [move-amount from to]]
-  (let [popped (take-last move-amount (stacks (dec from)))]
+  (let [popped (take move-amount (stacks (dec from)))]
     (-> stacks
-        (update (dec from) #(drop-last move-amount %))
-        (update (dec to) concat popped))))
+        (update (dec from) #(drop move-amount %))
+        (update (dec to) #(concat popped %)))))
 
 (defn find-phrase [operation data]
   (let [[stack-str operations-str] (str/split data #"\n\n")
         operations (partition 3 (map parse-long (re-seq #"\d+" operations-str)))
         stack (build-stacks stack-str)]
     (->> (reduce operation stack operations)
-         (map last)
+         (map first)
          (apply str))))
 
 (defn part-1 [data]
@@ -46,5 +41,5 @@
   (find-phrase operate-on-many data))
 
 (comment
-  (time (part-1 (slurp "input/2022/05.txt")))
-  (time (part-2 (slurp "input/2022/05.txt"))))
+  (part-1 (slurp "input/2022/05.txt"))
+  (part-2 (slurp "input/2022/05.txt")))
