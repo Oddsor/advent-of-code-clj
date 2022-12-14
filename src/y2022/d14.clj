cb6a8809d5d2906f1c6e0373f9200ef8d89fb804
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-14 12:43:24 +0100

    Y2022D14 part 1

--- src/y2022/d14.clj
+++ src/y2022/d14.clj
@@ -1,4 +1,4 @@
-(ns y2022.d14 
+(ns y2022.d14
   (:require [clojure.string :as str]))
 
 (def test-data "498,4 -> 498,6 -> 496,6
@@ -21,13 +21,42 @@
                (= ax bx) (for [y (range (min ay by) (inc (max ay by)))]
                            [ax y])
                (= ay by) (for [x (range (min ax bx) (inc (max ax bx)))]
-                           [x ay]))) 
+                           [x ay])))
            (partition 2 1 line))))
 
 (defn place-rocks [lines]
-  (reduce (fn [acc line] 
+  (reduce (fn [acc line]
             (into acc (map vector (line->coordinates line) (repeat :rock)))) {} lines))
 
+(defn next-position [[x y] m]
+  (let [possible-positions (remove #(m %) [[x (inc y)] [(dec x) (inc y)] [(inc x) (inc y)]])]
+    (if (empty? possible-positions)
+      [x y]
+      (first possible-positions))))
+
+(defn place-sand [max-y m]
+  (loop [pos [500 0]]
+    (if-let [[_ y :as npos] (next-position pos m)]
+      (if (or (= npos pos) (= max-y y))
+        (assoc m npos :sand)
+        (recur npos))
+      m)))
+
+(defn part-1 [data]
+  (let [lines (parse data)
+        initial-map (place-rocks lines)
+        [_ max-y _ _] (map-dimensions lines)]
+    (->> (loop [m initial-map]
+           (let [nm (place-sand max-y m)]
+             (if (or (= nm m)
+                     (some (fn [[[_ y] v]]
+                             (and (= :sand v)
+                                  (= max-y y))) nm))
+               nm
+               (recur nm))))
+         vals
+         (filter #{:sand})
+         count dec)))
 
 ;; Map with coordinate->rock
 (place-rocks (parse test-data))
