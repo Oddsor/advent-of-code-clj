8db07f9d9978e20fc4d4a578565ee4bad9fa3e69
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-18 11:30:44 +0100

    Y2022D18 twice as fast! Still slow

--- src/y2022/d18.clj
+++ src/y2022/d18.clj
@@ -1,5 +1,6 @@
 (ns y2022.d18
-  (:require [clojure.core.matrix :as m]))
+  (:require [clojure.core.matrix :as m]
+            [criterium.core :as crit]))
 
 (def test-data "2,2,2
 1,2,2
@@ -18,18 +19,19 @@
 (defn parse [data]
   (mapv vec (partition 3 (map parse-long (re-seq #"\d+" data)))))
 
+(defn neighbours [[head & points]]
+  (lazy-cat
+   (keep (fn [point] (when (>= 1.0 (m/distance head point))
+                       [head point])) points)
+   (when (seq points)
+     (neighbours points))))
+
 (defn part-1 [data]
   (let [points (parse data)
-        total-sides (* (count points) 6)]
+        total-sides (* (count points) 6)] 
     (- total-sides
-       (* (count (set (for [a points
-                            b points
-                            :when (and
-                                   (not= a b)
-                                   (>= 1.0 (m/distance a b)))]
-                        #{a b})))
-           2))))
+       (* (count (neighbours points)) 2))))
 
 (comment
-  (part-1 (slurp "input/2022/18.txt")))
+  (time (part-1 (slurp "input/2022/18.txt"))))
 (assert (= 64 (part-1 test-data)))
