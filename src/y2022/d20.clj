aee8b4f50e36ef223a47c740994ff6f5ab531d84
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2022-12-20 19:48:33 +0100

    Y2022D20 - faster version with mutable list

--- src/y2022/d20.clj
+++ src/y2022/d20.clj
@@ -1,12 +1,13 @@
-(ns y2022.d20 
+(ns y2022.d20
   (:require [clojure.edn :as edn]
-            [com.rpl.specter :refer :all]
-            [criterium.core :as crit]))
+            [com.rpl.specter :refer [AFTER-ELEM before-index NONE nthpath
+                                     setval]])
+  (:import (java.util ArrayList)))
 
 (defn parse [data]
   (edn/read-string (str "[" data "]")))
 
-(defn reorder 
+(defn reorder
   ([xs] (reorder 1 xs))
   ([times xs]
    (let [size (count xs)
@@ -22,10 +23,29 @@
                      new-idx (mod (+ idx n) (dec size))]
                  (->> acc
                       (setval [(nthpath idx)] NONE)
-                      (setval [(if (zero? new-idx) 
+                      (setval [(if (zero? new-idx)
                                  AFTER-ELEM
-                                 (before-index new-idx))] entry)
-                      vec)))) with-ids)
+                                 (before-index new-idx))] entry))))) with-ids)
+          (mapv :number)))))
+
+(defn reorder-fast
+  ([xs] (reorder-fast 1 xs))
+  ([times xs]
+   (let [size (count xs)
+         with-ids (map-indexed (fn [idx n] {:id idx :number n}) xs)]
+     (->> (if (> times 1)
+            (take (* size times) (cycle with-ids))
+            with-ids)
+          (reduce
+           (fn [^ArrayList acc {n :number :as entry}]
+             (if (zero? n)
+               acc
+               (let [idx (.indexOf acc entry)
+                     new-idx (mod (+ idx n) (dec size))]
+                 (doto acc
+                   (.remove ^int idx)
+                   (.add (if (zero? new-idx)
+                           (dec size) new-idx) entry))))) (ArrayList. with-ids))
           (mapv :number)))))
 
 (defn compute [xs]
@@ -34,13 +54,17 @@
                      (take-nth 1000)
                      (take 4)) + (cycle xs))))
 
-(defn part-1 [data] (compute (reorder (parse data))))
+(def ^:dynamic *reorder-function* reorder)
+
+(defn part-1 [data] (compute (*reorder-function* (parse data))))
 (defn part-2 [data] (->> data
                          parse
                          (mapv (partial * 811589153))
-                         (reorder 10)
+                         (*reorder-function* 10)
                          compute))
 
 (comment
-  (part-1 (slurp "input/2022/20.txt"))
-  (part-2 (slurp "input/2022/20.txt")))
+  (time (part-1 (slurp "input/2022/20.txt")))
+  (time (part-2 (slurp "input/2022/20.txt")))
+  (with-bindings {#'*reorder-function* reorder-fast} (part-1 (slurp "input/2022/20.txt")))
+  (with-bindings {#'*reorder-function* reorder-fast} (part-2 (slurp "input/2022/20.txt"))))
--- test/y2022/d20_test.clj
+++ test/y2022/d20_test.clj
@@ -1,6 +1,6 @@
 (ns y2022.d20-test
   (:require [clojure.test :refer [deftest is]]
-            [y2022.d20 :refer [part-1 part-2]]))
+            [y2022.d20 :refer [reorder-fast *reorder-function* part-1 part-2]]))
 
 (def test-data "1
 2
@@ -11,6 +11,8 @@
 4")
 
 (deftest part-1-test
-  (is (= 3 (part-1 test-data))))
+  (is (= 3 (part-1 test-data)))
+  (is (= 3 (with-bindings {#'*reorder-function* reorder-fast} (part-1 test-data)))))
 (deftest part-2-test
-  (is (= 1623178306 (part-2 test-data))))
+  (is (= 1623178306 (part-2 test-data)))
+  (is (= 1623178306 (with-bindings {#'*reorder-function* reorder-fast} (part-2 test-data)))))
