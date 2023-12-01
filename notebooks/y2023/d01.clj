e240ad21d8841462f8cf84dd93b1e2130510b2ae
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-12-01 17:04:33 +0100

    Y2023D01 formatting

--- notebooks/y2023/d01.clj
+++ notebooks/y2023/d01.clj
@@ -1,6 +1,7 @@
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (ns y2023.d01
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [nextjournal.clerk :as clerk]))
 
 ;; # Year 2023, day 1
 
@@ -19,24 +20,39 @@ treb7uchet")
 ;; and last digit so that they combine into a two-digit number
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
-(def first-and-last (juxt first last))
+(defn find-digits [text] (re-seq #"\d" text))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/example
+ (map find-digits (str/split-lines test-data)))
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
-(defn find-digits [text] (re-seq #"\d" text))
+(def first-and-last (juxt first last))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/example
+ (first-and-last ["1" "5" "3" "2"]))
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (def combine-first-and-last-digit (comp parse-long str/join first-and-last find-digits))
 
-(= 142 (->> test-data str/split-lines (map combine-first-and-last-digit) (apply +)))
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/example
+ (combine-first-and-last-digit "pqr3stu8vwx"))
+
+;; With helpers in hand we are able to solve the problem for the test data:
+
+(= 142 (transduce (map combine-first-and-last-digit)
+                  +
+                  (str/split-lines test-data)))
 
 ;; Solve for the input:
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (comment
-  (= 56049 (->> (slurp "input/2023/d01.txt")
-                str/split-lines
-                (map combine-first-and-last-digit)
-                (apply +))))
+  (= 56049 (transduce (map combine-first-and-last-digit)
+                      +
+                      (str/split-lines (slurp "input/2023/d01.txt")))))
 
 ;; ## Part 2
 
@@ -74,21 +90,20 @@ zoneight234
         digits (map (some-fn string-to-num identity) digits-possibly-worded)]
     (->> digits first-and-last str/join parse-long)))
 
-(= 281 (->> test-data-2 str/split-lines (map find-digits-worded) (apply +)))
+(= 281 (transduce (map find-digits-worded) + (str/split-lines test-data-2)))
 
 ;; Great. It works! We can now run this on the input:
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (comment
   ;; WRONG!
-  (= 54538 (->> (slurp "input/2023/d01.txt")
-                str/split-lines
-                (map find-digits-worded)
-                (apply +))))
+  (= 54538 (transduce (map find-digits-worded)
+                      +
+                      (str/split-lines (slurp "input/2023/d01.txt")))))
 
 ;; Oops! I'm apparently not correct, the answer is too high!
 ;; Turns out that naively using regex for this task ends up not working for strings where some worded numbers
-;; overlap. For example, the string `oneeightwo` returns the numbers `[1, 8]` as the first and last digit,
+;; overlap. For example, the string `oneeightwo` returns the numbers `[1,8]` as the first and last digit,
 ;; when it should've returned `[1,2]`.
 
 ;; Apparently the problem can be solved by using ["positive lookahead"](https://www.baeldung.com/java-regex-lookahead-lookbehind),
@@ -108,7 +123,6 @@ zoneight234
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (comment
   ;; Correct!
-  (= 54530 (->> (slurp "input/2023/d01.txt")
-                str/split-lines
-                (map find-digits-worded-overlapping)
-                (apply +))))
+  (= 54530 (transduce (map find-digits-worded-overlapping)
+                      +
+                      (str/split-lines (slurp "input/2023/d01.txt")))))
