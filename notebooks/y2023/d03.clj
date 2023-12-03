47bfcc8d97f3bcd1b4a5a0c8e175f7916771a713
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-12-03 22:37:52 +0100

    Y2023D03

---
+++ notebooks/y2023/d03.clj
@@ -0,0 +1,103 @@
+(ns y2023.d03
+  (:require [clojure.string :as str]
+            [clojure.core.matrix :as m]
+            [advent-of-code-clj.utils :as u]))
+
+(def test-data "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..")
+
+(defn text->matrix [text]
+  (->> text str/split-lines (mapv #(str/split % #""))))
+
+(defn is-symbol? [x] (re-matches #"[^\d\.]" x))
+
+(defn is-digit? [character] (re-matches #"\d" character))
+
+(defn digit-coordinates [matrix]
+  (filter #(is-digit? (get-in matrix %)) (m/index-seq matrix)))
+
+(defn group-number-coordinates [coords]
+  (when-let [xs (seq coords)]
+    (lazy-seq
+     (let [[fst & xss] xs
+           grouping (loop [[py px] fst
+                           [[ny nx :as n] & xrs] xss
+                           acc [fst]]
+                      (if (and (= py ny)
+                               (= (dec nx) px))
+                        (recur n xrs (conj acc n))
+                        acc))]
+       (cons grouping (group-number-coordinates (drop (count grouping) coords)))))))
+
+(->> test-data text->matrix digit-coordinates group-number-coordinates)
+(->> (slurp "input/2023/d03.txt") text->matrix digit-coordinates group-number-coordinates)
+
+(defn symbol-coordinates [matrix]
+  (into {} (for [y (range (count matrix))
+                 x (range (count (get matrix y)))
+                 :let [s (get-in matrix [y x])]
+                 :when (is-symbol? s)]
+             {[y x] s})))
+
+(defn adjacent-symbol? [symbol-coords [y x]]
+  (some symbol-coords (u/adjacent y x)))
+
+(let [matrix (-> test-data text->matrix)
+      symbol-coords (symbol-coordinates matrix)]
+  [(adjacent-symbol? symbol-coords [3 7])
+   (adjacent-symbol? symbol-coords [3 8])])
+
+(defn filter-number-coords [symbol-coords coords]
+  (filter #(some (partial adjacent-symbol? symbol-coords) %) coords))
+
+(defn coords->numbers
+  ([matrix]
+   (map (partial map #(get-in matrix %))))
+  ([matrix coords]
+   (map (partial map #(get-in matrix %)) coords)))
+
+(defn part-1 [data]
+  (let [matrix (-> data text->matrix)
+        symbol-coords (symbol-coordinates matrix)]
+    (->> matrix
+         digit-coordinates
+         group-number-coordinates
+         (filter-number-coords symbol-coords)
+         (coords->numbers matrix)
+         (map (partial apply str))
+         (map parse-long)
+         (apply +))))
+
+(= 4361 (part-1 test-data))
+(comment
+  (= 544433 (part-1 (slurp "input/2023/d03.txt"))))
+
+(defn part-2 [data]
+  (let [matrix (-> data text->matrix)
+        asterisk-coords (into {} (filter (comp #{"*"} val)) (symbol-coordinates matrix))
+        number-coords (-> matrix digit-coordinates group-number-coordinates)
+        relevant-coords (->> number-coords (filter-number-coords asterisk-coords))]
+    (->> asterisk-coords
+         keys
+         (map (fn [k]
+                (let [adj (set (cons k (apply u/adjacent k)))]
+                  (->> relevant-coords
+                       (filter #(some adj %))
+                       (coords->numbers matrix)
+                       (map str/join)
+                       (map parse-long)))))
+         (filter #(= 2 (count %)))
+         (map (partial apply *))
+         (apply +))))
+
+(= 467835 (part-2 test-data))
+(comment
+  (= 76314915 (part-2 (slurp "input/2023/d03.txt"))))
