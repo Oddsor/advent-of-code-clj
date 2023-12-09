a76d90b218097d0e40828c715a70586893603f4b
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-12-09 19:03:44 +0100

    Y2023D09 iteration

--- notebooks/y2023/d09.clj
+++ notebooks/y2023/d09.clj
@@ -5,24 +5,40 @@
 1 3 6 10 15 21
 10 13 16 21 30 45")
 
+;; Create a helper function to extract all numbers (possibly negative) from a piece of text
+
 (defn get-numbers [line] (mapv parse-long (re-seq #"-?\d+" line)))
+
+;; Create a function to find the difference between each number in a sequence.
+;; Creates a new sequence that is one element shorter in length to the original.
+
 (defn diff [xs] (map (fn [[a b]] (- b a)) (partition 2 1 xs)))
 
-(defn extrapolated [xs]
-  (if (every? zero? xs)
-    0
-    (+ (last xs) (extrapolated (diff xs)))))
+;; Here we're testing using the flexible iteration function to create the
+;; "difference-pyramid". Turns out it's a little bit faster than recursion (22 vs 16 ms).
+
+(defn extrapolated-iteration [xs]
+  (->> (seq (iteration identity
+                       :initk xs
+                       :vf last
+                       :kf diff
+                       :somef #(not-every? zero? %)))
+       reverse
+       (reduce + 0)))
 
 (defn part-1 [data]
   (transduce
    (comp (map get-numbers)
-         (map extrapolated))
+         (map extrapolated-iteration))
    +
    (str/split-lines data)))
 
 (= 114 (part-1 test-data))
 (comment
   (= 1904165718 (part-1 (slurp "input/2023/d09.txt"))))
+
+;; Part 2 is simply doing the same process, but adding a number to the
+;; start of the sequence instead of the end. This time we use recursion:
 
 (defn extrapolated-reverse [xs]
   (if (every? zero? xs)
