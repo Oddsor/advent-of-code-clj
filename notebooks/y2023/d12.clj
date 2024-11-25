7947785cf0f53b77850b597c7abec1f10385884d
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-11-25 16:15:03 +0100

    wip y2023d12

--- notebooks/y2023/d12.clj
+++ notebooks/y2023/d12.clj
@@ -1,7 +1,8 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
 (ns y2023.d12
   (:require [clojure.string :as str]))
 
-;; Year 2023; Day 12
+;; # Year 2023; Day 12
 
 ;; Here we need to generate various combinations of a string, where '?' can represent either a dot or hash symbol.
 ;; The combinations need to match a particular sequence of hash-symbols separated by dots, given by the numbers
@@ -31,27 +32,46 @@
 ;; This function splits a line, generates a regex-pattern from the right hand part (numbers), and a list of
 ;; combinations from the right hand part. Then we find the list of combinations that match the pattern:
 
-^{:nextjournal.clerk/visibility {:result :hide}}
-(defn find-arrangements [line]
-  (let [[xs counts] (str/split line #"\s")
-        pattern (re-pattern (str "^\\.*" (str/join "\\.+" (map (fn [x]
-                                                                 (str "#{" x "}")) (re-seq #"\d+" counts)))
-                                 "\\.*$"))]
-    (count (keep (comp (partial re-matches pattern) str/join) (combinations xs)))))
+(defn split-line
+  ([line] (split-line 1 line))
+  ([scale line]
+   (let [split (String/.split line " ")]
+     [(String/join "?" (repeat scale (aget split 0)))
+      (String/join "," (repeat scale (aget split 1)))])))
 
-(= 1 (find-arrangements "???.### 1,1,3"))
-(= 4 (find-arrangements ".??..??...?##. 1,1,3"))
-(= 1 (find-arrangements "?#?#?#?#?#?#?#? 1,3,1,6"))
-(= 10 (find-arrangements "?###???????? 3,2,1"))
+^{:nextjournal.clerk/visibility {:result :hide}}
+(defn find-arrangements [[xs counts]]
+  (let [pattern (re-pattern (str "^\\.*" (String/join "\\.+" (map (fn [x]
+                                                                    (str "#{" x "}")) (re-seq #"\d+" counts)))
+                                 "\\.*$"))]
+    (count (keep (partial re-matches pattern) (combinations xs)))))
+
+(= 1 (find-arrangements (split-line "???.### 1,1,3")))
+(= 4 (find-arrangements (split-line ".??..??...?##. 1,1,3")))
+(= 1 (find-arrangements (split-line "?#?#?#?#?#?#?#? 1,3,1,6")))
+(= 10 (find-arrangements (split-line "?###???????? 3,2,1")))
 
 ;; ## Part 1
-
 ;; Solution for the test set:
 
-(= 21 (apply + (map find-arrangements (str/split-lines test-data))))
+(= 21 (transduce (comp (map split-line)
+                       (map find-arrangements))
+                 + (str/split-lines test-data)))
 
 ;; This gives us the following solution for the input (use pmap to parallelize and speed things up):
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (comment
-  (= 7771 (apply + (pmap find-arrangements (str/split-lines (slurp "input/2023/d12.txt"))))))
+  (= 7771 (apply + (pmap (comp find-arrangements split-line) (str/split-lines (slurp "input/2023/d12.txt"))))))
+
+;; ## Part 2
+
+;; Oops, we need to scale the problem 5x by repeating the patterns!
+
+(mapv #(split-line 5 %) (str/split-lines test-data))
+
+(comment
+  (= 525152
+     (transduce (comp (map #(split-line 2 %))
+                      (map find-arrangements))
+                + (str/split-lines test-data))))
