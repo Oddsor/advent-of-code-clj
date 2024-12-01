594c18704d39e216a77dc11f0f019a6bc9f9a84e
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-12-01 07:47:28 +0100

    Y2024 D01

---
+++ notebooks/y2024/d01.clj
@@ -0,0 +1,68 @@
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(ns y2024.d01)
+
+; # Y2024, D01
+
+; Denne oppgaven er relativt enkel, men ser ut til å være
+; litt uvanlig i parsingen ettersom man har to lister som
+; går vertikalt i inputten:
+
+(def test-data "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+; ## Del 1
+
+; Vi skal finne distansene mellom verdiene i hver liste,
+; sortert fra lavest til høyest.
+
+; Vi kan starte med å få tak i listene:
+
+^{:nextjournal.clerk/visibility {:result :hide}}
+(require '[clojure.string :as str])
+^{:nextjournal.clerk/visibility {:result :hide}}
+(defn get-lists [input]
+  (->> (str/split-lines input)
+       (mapv (fn [line]
+               (mapv parse-long (re-seq #"\d+" line))))
+       (apply mapv vector)))
+
+(get-lists test-data)
+
+; Når vi har listene kan vi sortere og finne differansen
+; mellom hvert siffer, og summere:
+
+^{:nextjournal.clerk/visibility {:result :hide}}
+(defn difference [input]
+  (->> (get-lists input)
+       (map sort)
+       (apply map (comp Math/abs -))
+       (reduce +)))
+
+(= 11 (difference test-data))
+
+^{:nextjournal.clerk/visibility {:result :hide}}
+(comment
+  (= 2756096 (difference (slurp "input/2024/input1.txt"))))
+
+; ## Del 2
+
+; Denne gangen finner vi antall ganger et siffer i den første
+; listen dukker opp i den andre:
+
+^{:nextjournal.clerk/visibility {:result :hide}}
+(defn similarity-score [input]
+  (let [[list-a list-b] (get-lists input)
+        freqs (frequencies list-b)]
+    (transduce (map (fn [x] (* x (freqs x 0))))
+               +
+               list-a)))
+
+(= 31 (similarity-score test-data))
+
+^{:nextjournal.clerk/visibility {:result :hide}}
+(comment
+  (= 23117829 (similarity-score (slurp "input/2024/input1.txt"))))
