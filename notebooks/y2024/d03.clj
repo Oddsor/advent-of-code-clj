7b4f8f215dbc9b3db8261f14674982b657f58c76
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-12-03 13:06:14 +0100

    Regal-regex for fun

--- notebooks/y2024/d03.clj
+++ notebooks/y2024/d03.clj
@@ -6,7 +6,14 @@
 ; ## Del 1
 
 ; Starter med en enkel regex for å finne alle gyldige
-; multiplikasjoner:
+; multiplikasjoner. Denne gangen tester vi om regal-biblioteket
+; gjør ting mer lesbart..?
+
+(require '[lambdaisland.regal :as regal])
+(def mul-pattern
+  (regal/regex [:cat "mul("
+                [:capture [:+ :digit]] ","
+                [:capture [:+ :digit]] ")"]))
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (defn parse-and-multiply [text]
@@ -14,7 +21,7 @@
                     (* (Integer/parseInt num1)
                        (Integer/parseInt num2))))
              +
-             (re-seq #"mul\((\d+),(\d+)\)" text)))
+             (re-seq mul-pattern text)))
 
 (= 161 (parse-and-multiply "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
 
@@ -35,7 +42,11 @@
                      (* (Integer/parseInt num1)
                         (Integer/parseInt num2)))))
              +
-             (re-seq #"(don't\(\).*?do\(\)|(mul)\((\d+),(\d+)\))" text)))
+             (re-seq (regal/regex [:capture
+                                   [:alt
+                                    [:cat "don't()" [:*? :any] "do()"]
+                                    [:cat [:capture "mul"] "(" [:capture [:+ :digit]] "," [:capture [:+ :digit]] ")"]]])
+                     text)))
 
 (= 48 (parse-and-multiply-when "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"))
 
@@ -43,13 +54,19 @@
 (comment
   (= 98059175 (parse-and-multiply-when (slurp "input/2024/input3.txt"))))
 
-; Reell input ble regnet feil!! Da får vi gå dumt til verks.
+; Men reell input ble regnet feil!! Da får vi gå dumt til verks.
 
 ; Hent alle operasjoner vi bryr oss om:
 
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (defn get-operations [text]
-  (re-seq #"(don't\(\)|do\(\)|mul\((\d+),(\d+)\))" text))
+  (re-seq (regal/regex [:capture
+                        [:alt
+                         "don't()"
+                         "do()"
+                         [:cat "mul("
+                          [:capture [:+ :digit]] ","
+                          [:capture [:+ :digit]] ")"]]]) text))
 
 (get-operations "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
@@ -84,11 +101,16 @@
 ; Etter å ha sett løsningen til https://github.com/zelark/, så kan
 ; det være enklere å "preppe" inputten og gjenbruke funksjonen fra del 1:
 
+(def prep-pattern (regal/pattern [:cat
+                                  "don't()"
+                                  [:*? [:alt :any :newline]]
+                                  [:alt "do()" :end]]))
+
 ^{:nextjournal.clerk/visibility {:result :hide}}
 (defn prep-input [input]
   ; ?s slik at `.` matcher newline også
   ;`\Z` for å matche slutten av filen (fanger opp `don't` som ikke følges av en `do`)
-  (.replaceAll input "(?s)don't\\(\\).*?(?:do\\(\\)|\\Z)" ""))
+  (.replaceAll input prep-pattern ""))
 
 (prep-input "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?don't()mul(8,5))")
 
