e215005a81d8c1cc224e5861fc25e729aefd1fd4
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-06-29 11:11:59 +0200

    Y2024D15 med eksempler

--- notebooks/y2024/d15.clj
+++ notebooks/y2024/d15.clj
@@ -79,13 +79,14 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
     \v [(inc old-y) old-x]
     \< [old-y (dec old-x)]))
 
-(defn print-board-cm [{:keys [coord-map robot]}]
+(defn print-board [{:keys [coord-map robot]}]
   (let [dimensions [(inc (apply max (map first (keys coord-map))))
                     (inc (apply max (map second (keys coord-map))))]]
-    (mx/pm (mx/emap-indexed (fn [idx _]
-                              (if (= idx robot) \@
-                                  (coord-map idx)))
-                            (apply mx/new-matrix dimensions)))))
+    (with-out-str
+      (mx/pm (mx/emap-indexed (fn [idx _]
+                                (if (= idx robot) \@
+                                    (coord-map idx)))
+                              (apply mx/new-matrix dimensions))))))
 
 ; Denne algoritmen gir en score til en gitt boks:
 
@@ -129,6 +130,44 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
              :robot new-robot-position
              :coord-map (update-map coord-map new-locations move)))))
 
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+...O...
+...@...
+......#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt opp mot vegg, ingen effekt"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \^))]]])
+
+; Hvis vi flytter en boks som ikke står mot en vegg:
+
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+.......
+...O...
+...@..#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt opp, flytter også boks"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \^))]]])
+
+; Hvis vi flytter flere bokser som ikke står mot en vegg:
+
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+.......
+...O...
+...O...
+...@..#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt opp, flytter alle bokser"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \^))]]])
+
 (defn solve [{:keys [game-state moves]}]
   (->> (reduce step game-state moves)
        :coord-map
@@ -156,17 +195,59 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
 (defn expand-board [input]
   (str/escape input {\# "##" \O "[]" \. ".." \@ "@."}))
 
-^{:kind/hiccup true}
+^{:kind/hiccup true :kindly/hide-code true}
 [:div {:style {:display "grid" :grid-template-columns "1fr 1fr"}}
- [:h3 "Før"]
- [:h3 "Etter"]
+ [:h3 {:style {:grid-column "1 / span 2"}} "Utvidet testdata"]
  [:code [:pre test-data-small]]
  [:code [:pre (expand-board test-data-small)]]]
 
 ; Vi kan parse kartet på omtrent samme måte som tidligere, selv om boksene
 ; er bredere, så lenge `map-and-moves` tolker både `[]` og `O` som bokser.
 
-; Og da kan vi løse del 2:
+; Eksempel på flytting av flere bokser i et diamant-mønster:
+
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+.......
+...[]...
+..[][]..
+...[]...
+...@..#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt opp, flytter alle bokser"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \^))]]])
+
+; Bredde-først-søket må ikke fange opp nabo-bokser som ikke skal bli flyttet:
+
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+.......
+...[]...
+..[][]..
+..@[]...
+......#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt opp, flytter ikke boksene til høyre"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \^))]]])
+
+^{:kind/hiccup true :kindly/hide-code true}
+(let [game-state (:game-state (map-and-moves "...#...
+.......
+...[]...
+.@[][]..
+...[]...
+......#"))]
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr"}}
+   [:h3 {:style {:grid-column "1 / span 2"}} "Flytt høyre, flytter de midterste boksene"]
+   [:code [:pre (print-board game-state)]]
+   [:code [:pre (print-board (step game-state \>))]]])
+
+; Når vi er sikre på at algoritmen fungerer kan vi løse del 2:
 
 (solve (map-and-moves (expand-board test-data-large)))
 
