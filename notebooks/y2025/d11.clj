e1fbd983a10b50ad6b60842abb34a8bddf1ef6f1
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2025-12-13 00:58:13 +0100

    Y2025D11 p2 too slow

--- notebooks/y2025/d11.clj
+++ notebooks/y2025/d11.clj
@@ -1,7 +1,8 @@
 (ns y2025.d11 
   (:require
    [advent-of-code-clj.utils :as utils]
-   [advent-of-code-clj.input :as input]))
+   [advent-of-code-clj.input :as input]
+   [medley.core :as medley]))
 
 (def test-input "aaa: you hhh
 you: bbb ccc
@@ -15,12 +16,15 @@ hhh: ccc fff iii
 iii: out")
 
 (defn input->edges [input]
-  (let [lines (utils/->lines (String/.replace input ":" ""))]
-    (->> lines
-         (map (fn [line]
-                (let [[src & targets] (String/.split line " ")]
-                  [(keyword src) (map keyword targets)])))
-         (into {}))))
+  (->> (String/.replace input ":" "")
+       utils/->lines
+       (map (fn [line]
+              (let [[src & targets] (String/.split line " ")]
+                [(keyword src) (map keyword targets)])))
+       (into {})))
+
+(defn valid-trail? [trail]
+  (every? #(= 1 (val %)) (frequencies trail)))
 
 (defn find-all-paths [edges start end]
   (let [expand-trail (fn [trail]
@@ -29,7 +33,9 @@ iii: out")
                          (map (fn [t] (conj trail t)) next-tips)))]
     (loop [unfinished-trails #{[start]}
            finished-trails #{}]
-      (let [next-trails (mapcat expand-trail unfinished-trails)
+      (let [next-trails (->> unfinished-trails
+                             (mapcat expand-trail)
+                             (filter valid-trail?)) 
             {f true u false} (group-by (comp boolean #{end} peek)
                                        next-trails)
             n-finished (into finished-trails f)]
@@ -44,3 +50,38 @@ iii: out")
 (part-1 test-input)
 
 (part-1 (input/get-input 2025 11))
+
+(def test-input-2 "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out")
+
+(defn remove-edges [edge-to-remove edges]
+  (medley/map-vals (fn [outbound]
+                     (vec (remove edge-to-remove outbound)))
+                   edges))
+
+(defn part-2 [input]
+  (let [edges (input->edges input) 
+        paths-containing-intermediate-stops
+        (filter (fn [path]
+                  (and (some #{:fft} path)
+                       (some #{:dac} path)))
+                (find-all-paths edges
+                                :svr :out))]
+    (count paths-containing-intermediate-stops)))
+
+(time
+  (part-2 test-input-2))
+
+;; TODO too slow
+#_(delay (time (part-2 (input/get-input 2025 11))))
